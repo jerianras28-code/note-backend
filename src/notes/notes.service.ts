3d4712aa4f00d2c50a10d8f86ebcea66ef4548fb
Jerian Ras <jerianras28@gmail.com>
2026-03-04 22:49:57 -0400

    added new functions and data to Injectables in noteservice

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+  noteContainer: Array<string> = [];
+
+  getNotes(): string[] {
+    return this.noteContainer;
+  }
+
+  addNote(props: string): void {
+    this.noteContainer.push(props);
+  }
+
+  deleteNote(props: number): void {
+    this.noteContainer.splice(props, 1);
+  }
+}
